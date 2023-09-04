ae0d984abf8e640f04cb9073b95e682b4d673f57
Jeremy Skrdlant <jeremy.skrdlant@nwktc.edu>
2023-09-04 08:11:53 -0600

    Initial Commit

---
+++ Closure Tutorial/Closure_TutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  Closure_TutorialApp.swift
+//  Closure Tutorial
+//
+//  Created by Jeremy Skrdlant on 9/4/23.
+//
+
+import SwiftUI
+
+@main
+struct Closure_TutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
